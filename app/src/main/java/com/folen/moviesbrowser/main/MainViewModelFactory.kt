695dc6aafe76655a5195397d05d4fd9511ec072c
mosz_kamil <kamil.mosz@apagroup.pl>
2022-03-20 14:21:09 +0100

    Implemented REST communication with MovieDB API by using Retrofit, coroutines. Tried to implement it in MVVM pattern. Added some xml files to edit later such as dimens.xml. Updated libraries versions. Used SonarLint to find and fix all bugs, vulnerabilities and code smells. Added files that contains passwords to .gitignore.

---
+++ app/src/main/java/com/folen/moviesbrowser/main/MainViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.folen.moviesbrowser.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.folen.moviesbrowser.restapi.MovieRepository
+
+class MainViewModelFactory constructor(private val repository: MovieRepository) :
+    ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            MainViewModel(this.repository) as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+}
---
+++ app/src/main/java/com/folen/moviesbrowser/utilities/ConstantValues.kt
@@ -0,0 +1,8 @@
+package com.folen.moviesbrowser.utilities
+
+object ConstantValues {
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+    const val API_KEY = "36a9335b97de344e540bf46e01cbc67a"
+    const val DEFAULT_LANGUAGE = "en-US"
+    const val PAGE_REQUEST = 1
+}
